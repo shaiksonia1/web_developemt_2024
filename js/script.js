9bab423948c2350ae4a6eff9ac67877b7e7476cf
Shaik Sonia <shaiksonia222@gmail.com>
2024-10-28 03:50:44 +0530

    home

--- js/script.js
+++ js/script.js
@@ -41,6 +41,14 @@ gsap.to("#main",{
 
 
 
-function myFunction(x) {
-    x.classList.toggle("change");
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    const hamburger = document.getElementById("hamburger");
+  
+    // Toggle the visibility of the navigation links
+    navLinks.classList.toggle("show");
+  
+    // Toggle the "open" class on the hamburger icon to animate it
+    hamburger.classList.toggle("open");
   }
+  
