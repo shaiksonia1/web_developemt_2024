b38ae7091accd7879947e730cf9b0af7970ca03b
Shaik Sonia <shaiksonia222@gmail.com>
2024-10-28 02:09:13 +0530

    JS

--- js/script1.js
+++ js/script1.js
@@ -1,12 +1,12 @@
-var crsr = document.querySelector("#cursor");
-var blur = document.querySelector("#cursor-blur");
-document.addEventListener("mousemove", function(dets) {
-    crsr.style.left = dets.x + "px";
-    crsr.style.top = dets.y + "px"; 
-    blur.style.left = dets.x - 50 +"px";
-    blur.style.top = dets.y -200+ "px"; 
+// var crsr = document.querySelector("#cursor");
+// var blur = document.querySelector("#cursor-blur");
+// document.addEventListener("mousemove", function(dets) {
+//     crsr.style.left = dets.x + "px";
+//     crsr.style.top = dets.y + "px"; 
+//     blur.style.left = dets.x - 50 +"px";
+//     blur.style.top = dets.y -200+ "px"; 
 
-});
+// });
 
 
 gsap.to("#nav",{
