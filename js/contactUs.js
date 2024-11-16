850d3f1cb49b889ae9c9e68aaab7938c689ef2a7
Shaik Sonia <shaiksonia222@gmail.com>
2024-11-16 17:48:58 +0530

    update

---
+++ js/contactUs.js
@@ -0,0 +1,40 @@
+
+gsap.to("#nav",{
+    backgroundColor: "#191818",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start: "top -30vh",
+        end:"top -80%",
+        scrub:2
+    }
+})
+
+
+
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    const hamburger = document.getElementById("hamburger");
+  
+    // Toggle the visibility of the navigation links
+    navLinks.classList.toggle("show");
+  
+    // Toggle the "open" class on the hamburger icon to animate it
+    hamburger.classList.toggle("open");
+  }
