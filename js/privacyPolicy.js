be1d441d1bbd4aaed03cb70f09cb037862d5eb84
Shaik Sonia <shaiksonia222@gmail.com>
2024-11-28 21:31:44 +0530

    PrivacyPolicy

---
+++ js/privacyPolicy.js
@@ -0,0 +1,29 @@
+gsap.to("#nav",{
+    backgroundColor: "#191818",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start: "top -30vh",
+        end:"top -80%",
+        scrub:2
+    }
+})
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
